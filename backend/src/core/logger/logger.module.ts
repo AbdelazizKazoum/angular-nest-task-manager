71497e38147831e6fe68a9f2e9c3c5a05257fbe6
Abdelaziz_kazoum <abdukazoum@gmail.com>
2026-01-05 15:08:57 +0100

    feat: integrate Pino logger with configuration and update server port

--- backend/src/core/logger/logger.module.ts
+++ backend/src/core/logger/logger.module.ts
@@ -1,4 +1,23 @@
 import { Module } from '@nestjs/common';
 
-@Module({})
+import { LoggerModule as PinoLoggerModule } from 'nestjs-pino';
+
+@Module({
+  imports: [
+    PinoLoggerModule.forRoot({
+      pinoHttp: {
+        level: process.env.LOG_LEVEL || 'info',
+        transport: {
+          target: 'pino-pretty',
+          options: {
+            singleLine: true,
+            colorize: true,
+            translateTime: 'SYS:standard',
+            ignore: 'pid,hostname',
+          },
+        },
+      },
+    }),
+  ],
+})
 export class LoggerModule {}

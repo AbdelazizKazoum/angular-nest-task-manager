4aaa5cecbf4483c8bbae33a9f2fc93d2ab7fdb54
Abdelaziz KAZOUM <abdukazoum@gmail.com>
2026-01-28 20:35:15 +0100

    feat: set global API prefix and remove unused UsersModule

--- backend/src/main.ts
+++ backend/src/main.ts
@@ -6,6 +6,8 @@ import { ValidationPipe } from '@nestjs/common';
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
 
+  app.setGlobalPrefix('api');
+
   app.useGlobalPipes(new ValidationPipe({ whitelist: true, transform: true }));
 
   app.useLogger(app.get(Logger));
